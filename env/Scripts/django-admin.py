a0f7fbe6bfa3054639139c37b6e7b506ba948cbf
Sean <smccarth@ucsd.edu>
2020-04-26 03:37:12 -0700

    First commit- example

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Seanathon\Desktop\College\CSE Workpace\CSE 110\Cheap Trips\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
