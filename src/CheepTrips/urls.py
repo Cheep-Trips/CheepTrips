185f553c416bfd089c0e886c05985655e9dd821a
Aaron Nhan <aaronnhan11@gmail.com>
2020-04-28 14:09:41 -0700

    add user app

--- src/CheepTrips/urls.py
+++ src/CheepTrips/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from posts.views import post_list_view
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('trips/', include('trips.urls')),
+    path('', include('users.urls')), 
+    path('users/', include('users.urls')),
 ]
