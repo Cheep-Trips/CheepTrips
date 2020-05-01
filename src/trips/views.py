7cf30bb09825a89b42b3882164f44c6e5ecaf7e0
Aaron Nhan <aaronnhan11@gmail.com>
2020-05-01 16:53:25 -0700

    feat: prints Amadeus api calll

--- src/trips/views.py
+++ src/trips/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from amadeus import Client, ResponseError
+import os
 # Create your views here.
 def start(request):
-    return render(request, 'trips/start.html', {})
+
+    amadeus = Client(
+        client_id=os.getenv("AMADEUS_KEY"),
+        client_secret=os.getenv("AMADEUS_SECRET"),
+    )
+    try:
+        response = amadeus.shopping.flight_offers_search.get(
+            originLocationCode='SYD',
+            destinationLocationCode='BKK',
+            departureDate='2020-07-01',
+            adults=1)
+        return HttpResponse(response.data)
+    except ResponseError as error:
+        print(error)
+    return HttpResponse("<html>ERROR</html>")
+
+    #return render(request, 'trips/start.html', {})
 def dest(request):
     return render(request, 'trips/dest.html', {})
 def sign_in(request):
