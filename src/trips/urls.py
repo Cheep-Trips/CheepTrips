3e72fe1c3c8d23dc898106d4ec295018314a73c5
Aaron Nhan <aaronnhan11@gmail.com>
2020-05-02 19:51:29 -0700

    feat: added 5/2 meeting functionality

--- src/trips/urls.py
+++ src/trips/urls.py
@@ -4,10 +4,15 @@ from . import views
 
 app_name = 'trips'
 urlpatterns = [
-    path('', views.start, name='index'),
-    path('start', views.start, name='start'),
-    path('dest', views.dest, name='dest'),
+    path('', views.welcome, name='index'),
+    path('welcome', views.welcome, name='welcome'),
+    path('destination', views.destination, name='destination'),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('saved_trips/', views.saved_trips, name='saved_trips'),
     path('view_trip/', views.view_trip, name='view_trip'),
+    path('profile/', views.profile, name='profile'),
+    path('compare/', views.compare, name='compare'),
+    path('new_account/', views.new_account, name='new_account'),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('view_flight/', views.view_flight, name='view_flight'),
 ]
--- src/trips/views.py
+++ src/trips/views.py
@@ -3,29 +3,39 @@ from django.http import HttpResponse
 from amadeus import Client, ResponseError
 import os
 # Create your views here.
-def start(request):
+def welcome(request):
 
-    amadeus = Client(
-        client_id=os.getenv("AMADEUS_KEY"),
-        client_secret=os.getenv("AMADEUS_SECRET"),
-    )
-    try:
-        response = amadeus.shopping.flight_offers_search.get(
-            originLocationCode='SYD',
-            destinationLocationCode='BKK',
-            departureDate='2020-07-01',
-            adults=1)
-        return HttpResponse(response.data)
-    except ResponseError as error:
-        print(error)
-    return HttpResponse("<html>ERROR</html>")
+    # amadeus = Client(
+    #     client_id=os.getenv("AMADEUS_KEY"),
+    #     client_secret=os.getenv("AMADEUS_SECRET"),
+    # )
+    # try:
+    #     response = amadeus.shopping.flight_offers_search.get(
+    #         originLocationCode='SYD',
+    #         destinationLocationCode='BKK',
+    #         departureDate='2020-07-01',
+    #         adults=1)
+    #     return HttpResponse(response.data)
+    # except ResponseError as error:
+    #     print(error)
+    # return HttpResponse("<html>ERROR</html>")
 
-    #return render(request, 'trips/start.html', {})
-def dest(request):
-    return render(request, 'trips/dest.html', {})
+    return render(request, 'trips/welcome.html', {})
+def destination(request):
+    return render(request, 'trips/destination.html', {})
 def sign_in(request):
     return render(request, 'trips/sign_in.html', {})
 def saved_trips(request):
     return render(request, 'trips/saved_trips.html', {})
 def view_trip(request):
     return render(request, 'trips/view_trip.html', {})
+def profile(request):
+    return render(request, 'trips/profile.html', {})
+def compare(request):
+    return render(request, 'trips/compare.html', {})
+def new_account(request):
+    return render(request, 'trips/new_account.html', {})
+def forgot_password(request):
+    return render(request, 'trips/forgot_password.html', {})
+def view_flight(request):
+    return render(request, 'trips/view_flight.html', {})
