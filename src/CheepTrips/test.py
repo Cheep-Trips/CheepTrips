115f50e7ad4ec6ab7d4ad3e177844c3874f1d424
Matthew Zane <matthewdzane@gmail.com>
2020-05-07 01:35:29 -0700

    Fixed syntax errors in test.py

--- src/CheepTrips/test.py
+++ src/CheepTrips/test.py
@@ -1,14 +1,13 @@
 import unittest
 
-# just for testing unittest and Travis CI
+# just to test unittest and Travis CI
 class TestSum(unittest.TestCase):
-    # should pass
-    def test_sum(self):
-		self.assertEqual(sum([1, 2, 3]), 6, “Should be 6”)
 
-    # should fail
-	def test_sum_tupple(self):
-		self.assertEqual(sum([1. 2. 2]), 6, “Should be 6”)
+	def test_sum(self):
+		self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
-if __name__ == “__main__”:
+	def test_sum_tuple(self):
+		self.assertNotEqual(sum((1, 2, 2)), 6, "This should not be 6.")
+
+if __name__ == '__main__':
 	unittest.main()
