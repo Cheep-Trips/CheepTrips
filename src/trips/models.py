1ad86e662743deab24aca5b629826ed768d7219c
kfbach <kfbach@ucsd.edu>
2020-05-06 19:33:50 -0700

    pt 2

--- src/trips/models.py
+++ src/trips/models.py
@@ -3,41 +3,35 @@ from django.db import models
 # Create your models here.
 
 class Region(models.Model):
-    region = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     
 
 class Location(models.Model):
     exchange_rate = models.IntegerField(default=1)
     cost_of_living = models.IntegerField(default=0)
-    regions =  models.ForeignKey(Region, on_delete=models.CASCADE)
+    region =  models.ForeignKey(Region, on_delete=models.CASCADE)
 
-class Activities(models.Model):
-    activity = models.CharField(max_length=200)
+class Activity(models.Model):
+    name = models.CharField(max_length=200)
     locations = models.ManyToManyField(Location)
 
-class Flights(models.Model):
-    start_airport = models.CharField(max_length=200)
-    dest_airport = models.OneToOneField(
-        Location,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
+class Flight(models.Model):
+    departure_location = models.ForeignKey(Location, on_delete=models.CASCADE,related_name='departures')
+    destination = models.ForeignKey(Location, on_delete=models.CASCADE,related_name='arrivals')
     flight_carrier = models.CharField(max_length=200)
     departure_time = models.DateTimeField('Departure date')
     arrival_time = models.DateTimeField('Arrival date')
     num_passengers = models.IntegerField(default=1)
     num_bags = models.IntegerField(default=1)
 
-class Trips(models.Model):
+class Trip(models.Model):
+    name = models.CharField(max_length=200)
     budget = models.IntegerField(default=0)
-    total_cost = models.IntegerField(default=0)
-    flight_list = models.ManyToManyField(Flights)    
+    cost = models.IntegerField(default=0)
+    flights = models.ManyToManyField(Flight)    
 
     
-
-
-
-class User(models.Model):
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
+# class User(models.Model):
+#     email = models.CharField(max_length=200)
+#     password = models.CharField(max_length=200)
     
