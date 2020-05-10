50402917ad4d5d166439afff7d43dd4be69c5d93
Matthew Zane <matthewdzane@gmail.com>
2020-05-09 23:25:50 -0700

    Moved tests from CheepTrips to posts

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import unittest
 
-# Create your tests here.
+# just to test unittest and Travis CI
+class TestSum(unittest.TestCase):
+
+	def test_sum(self):
+		self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+	def test_sum_tuple(self):
+		self.assertNotEqual(sum((1, 2, 2)), 6, "This should not be 6.")
+
+if __name__ == '__main__':
+	unittest.main()
