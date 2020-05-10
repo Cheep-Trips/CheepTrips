9ae5fa6442191132c2b5b89d4e09fd8406ffbf73
Matthew Zane <matthewdzane@gmail.com>
2020-05-10 00:11:01 -0700

    Changed unittest to Django TestCase

--- src/posts/tests.py
+++ src/posts/tests.py
@@ -1,13 +1,10 @@
-import unittest
+from django.test import TestCase
 
-# just to test unittest and Travis CI
-class TestSum(unittest.TestCase):
+# just to test Django TestCase and Travis CI
+class TestSumDjango(TestCase):
 
-	def test_sum(self):
-		self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
-	def test_sum_tuple(self):
-		self.assertNotEqual(sum((1, 2, 2)), 6, "This should not be 6.")
-
-if __name__ == '__main__':
-	unittest.main()
+    def test_sum_tuple(self):
+        self.assertNotEqual(sum((1, 2, 2)), 6, "This should not be 6.")
