e94ca9bd694fad10b3385acd149ce60445633af2
Matthew Zane <matthewdzane@gmail.com>
2020-05-29 23:53:15 -0700

    Added stuff for admin site and made myself a superuser account

--- src/trips/admin.py
+++ src/trips/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Location)
+admin.site.register(Activity)
+admin.site.register(Flight)
+admin.site.register(Trip)
+admin.site.register(User)
